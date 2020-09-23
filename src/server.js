0c8b1ad591a915a8f0c9b2c7205a70cab3bd9af8
FelipeEnne <felipeenne@gmail.com>
2020-09-23 17:40:41 -0300

    config database

--- src/server.js
+++ src/server.js
@@ -2,10 +2,23 @@ const port = 3003;
 
 const express = require('express');
 const app = express();
+const database = require('./database')
 
 
-app.get('/products', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 1000 });
+app.get('/products', (req, res) => {
+    res.send(database.getProducts());
+})
+
+app.get('/products/:id', (req, res) => {
+    res.send(database.getProducts(req.params.id));
+})
+
+app.post('/products', (req, res) => {
+    const produto =  database.salvarProduct({
+        nome: req.body.name,
+        preco: req.body.preco,
+    });
+    res.send(produto);
 })
 
 app.listen(port, () => {
